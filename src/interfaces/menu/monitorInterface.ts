33edd9e3e160e3e45f06dec174b3045fb7d9c761
Myeong Han Park <malgirlim@gmail.com>
2023-02-02 08:52:29 +0900

    230202_park

--- src/interfaces/menu/monitorInterface.ts
+++ src/interfaces/menu/monitorInterface.ts
@@ -7,7 +7,13 @@ export interface MonitorStock {
   규격?: String;
   단위?: String;
   재고수?: Number;
-  비고?: String;
+  안전재고수?: Number;
+  부족재고수?: Number;
+  입고수?: Number;
+  출고수?: Number;
+  불량수?: Number;
+  원가?: Number;
+  재고금액?: Number;
 }
 
 // 재고 조회 - 원자재 불량 조회
@@ -21,19 +27,24 @@ export interface MonitorBad {
   불량수?: Number;
   불량명?: String;
   불량내용?: String;
-  비고?: String;
+  원가?: Number;
+  불량금액?: Number;
 }
 
 // 재고 조회 - 안전재고 미달 조회
 export interface MonitorSafe {
   NO?: Number;
-  폼목코드?: String;
+  품목코드?: String;
   거래처명?: String;
   품명?: String;
   규격?: String;
   단위?: String;
-  안전재고?: Number;
   재고수?: Number;
-  안전재고보유?: Boolean;
-  비고?: String;
+  안전재고수?: Number;
+  부족재고수?: Number;
+  입고수?: Number;
+  출고수?: Number;
+  불량수?: Number;
+  원가?: Number;
+  재고금액?: Number;
 }
