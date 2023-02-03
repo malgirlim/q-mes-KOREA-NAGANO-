988ed4a99f0731a5084a358ff6c316f4c1449992
KoBeomSeok <rhqjatjr300@gmail.com>
2023-02-03 17:59:09 +0900

    230203_bom 완료

--- backend/router/masterRouter.js
+++ backend/router/masterRouter.js
@@ -5,6 +5,9 @@ const masterProductRouter = require("./master/masterProduct");
 const masterClientRouter = require("./master/masterClient");
 const masterBadRouter = require("./master/masterBad");
 const masterUserRouter = require("./master/masterUser");
+const masterProcessRouter = require("./master/masterProcess");
+const masterLocationRouter = require("./master/masterLocation");
+const masterBomRouter = require("./master/masterBom");
 
 router.use((req, res, next) => {
   // console.log("middleware for test!");
@@ -19,5 +22,11 @@ router.use("/product", masterProductRouter);
 router.use("/client", masterClientRouter);
 // 기준정보 / 불량내용 등록 - router/master/masterBad.js
 router.use("/bad", masterBadRouter);
+// 기준정보 / 공정 등록 - router/master/masterProcess.js
+router.use("/process", masterProcessRouter);
+// 기준정보 / 원자재 위치 등록 - router/master/masterLocation.js
+router.use("/location", masterLocationRouter);
+// 기준정보 / BOM 등록 - router/master/masterBom.js
+router.use("/bom", masterBomRouter);
 
 module.exports = router;
