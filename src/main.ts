3000aea0e4c5f7b58241e9af2fee3f5fdbc39d7f
KoBeomSeok <rhqjatjr300@gmail.com>
2023-02-14 17:07:43 +0900

    230214

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
+import store from "./store";
 import Vue3Toasity from "vue3-toastify";
 import "vue3-toastify/dist/index.css";
 import "./assets/css/app.css";
 
 createApp(App)
   .use(router)
+  .use(store)
   .use(createPinia())
   .use(Vue3Toasity, {
     autoClose: 3000,
